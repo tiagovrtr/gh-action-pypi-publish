dcafd35c5e4f5187c3556d7c5aa7f75e9f19ecb9
Tiago Reis <tiagovrtreis@gmail.com>
2022-06-21 20:50:02 +0000

    Add release_version as output
    Signed-off-by: GitHub <noreply@github.com>

--- twine-upload.sh
+++ twine-upload.sh
@@ -52,3 +52,7 @@ TWINE_USERNAME="$INPUT_USER" \
 TWINE_PASSWORD="$INPUT_PASSWORD" \
 TWINE_REPOSITORY_URL="$INPUT_REPOSITORY_URL" \
   exec twine upload ${TWINE_EXTRA_ARGS} ${INPUT_PACKAGES_DIR%%/}/*
+
+if [[ ${OUTPUT_RELEASE_VERSION,} != "false" ]] ; then
+    echo "::set-output name=release_version::${INPUT_PACKAGES_DIR%%/s%.tar.gz}"
+fi
